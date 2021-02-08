f54ca254a5f91ca3b92a1995e37eb9d5db77c1be
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-08 04:33:07 +0000

    sendphoto by file_id

--- sendPhoto.py
+++ sendPhoto.py
@@ -5,13 +5,14 @@ from pprint import pprint
 # token = os.environ['token']
 token = '1646026624:AAFl-4-09PT5AM5T1RewNdGSo52jnLMLNv4'
 # 1258594598
+# AgACAgQAAxkBAAMOYCC-dNtXi3ZzJ8xHlMVXSVycWOsAAuyqMRtxmGxQW71jzwkrdqeImgABI10AAwEAAwIAA20AAz-IAAIeBA
 
 def sendPhoto():
     
     url = f'https://api.telegram.org/bot{token}/sendPhoto'
     payload = {
         'chat_id': 1258594598,
-        'photo': 'https://images.app.goo.gl/Dw9GVsbafCDKoaXN7',
+        'photo': 'AgACAgQAAxkBAAMOYCC-dNtXi3ZzJ8xHlMVXSVycWOsAAuyqMRtxmGxQW71jzwkrdqeImgABI10AAwEAAwIAA20AAz-IAAIeBA',
         'caption': 'masjid rasmi'
     }
     respons = requests.get(url=url, params=payload)
